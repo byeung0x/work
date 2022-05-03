a0d23c1c227d602311ff875b540bbca06bcfa0d4
byeung0x <94519768+byeung0x@users.noreply.github.com>
2022-05-03 22:30:39 +0800

    Update requests_basic.py

--- requests_basic.py
+++ requests_basic.py
@@ -3,6 +3,7 @@
 
 import requests,json
 
+#Example Used from HKO:https://www.hko.gov.hk/tc/weatherAPI/doc/files/HKO_Open_Data_API_Documentation_tc.pdf
 def _req(URL="https://data.weather.gov.hk/weatherAPI/opendata/weather.php",payload= {'dataType': 'flw', 'lang': 'tc'}):
     response = requests.get(URL,params=payload).json()
     return response
