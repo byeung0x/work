e81e807d9ab29f0fc35b5489a144ea49bfbe15ad
byeung0x <94519768+byeung0x@users.noreply.github.com>
2022-05-03 23:00:55 +0800

    Update requests_basic.py

--- requests_basic.py
+++ requests_basic.py
@@ -3,13 +3,27 @@
 
 import requests,json
 
+#1
+#Extract data from URL using HTTP GET method.
 #Example Used from HKO:https://www.hko.gov.hk/tc/weatherAPI/doc/files/HKO_Open_Data_API_Documentation_tc.pdf
 def _req(URL="https://data.weather.gov.hk/weatherAPI/opendata/weather.php",payload= {'dataType': 'flw', 'lang': 'tc'}):
     response = requests.get(URL,params=payload).json()
     return response
 
+#2
+#A light weight method to check validity of a request. Code 200 is all good. Code 401 usually means your authentication process failed.
+def _req_code(URL="https://data.weather.gov.hk/weatherAPI/opendata/weather.php",payload= {'dataType': 'flw', 'lang': 'tc'}):
+    status_code = requests.get(URL,params=payload).status_code
+    return status_code
+
 if __name__ == '__main__':
-    print(_req())
+    print(_req()) #1
+    print(_req_code()) #2
+    
+    
+    
+    
+  
     
     
     
